d2d072897aefef2e5c99f29a2de6a72e3e97326a
Daniel Potter <>
2021-02-16 23:56:02 -0800

    thought bubbles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useLayoutEffect, useState, createRef } from 'react'
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [thought, setThought] = useState('');
+  const inputRef = createRef();
+
+  useLayoutEffect(() => {
+    inputRef.current.focus();
+  }, [])
+
+  function valueChanged(event) {
+    setMessage(event.currentTarget.value);
+  }
+
+  function commitThought(event) {
+    if (event.key === 'Enter') {
+      setThought(message);
+      setMessage('');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{paddingLeft: '10%', paddingTop: '5%'}}>
+      <h1 style={{fontSize: '4em'}}>{thought}</h1>
+      <input type='text' 
+             value={message} 
+             ref={inputRef}
+             style={{position: 'absolute', left: '-9999px'}}
+             onChange={(event) => setMessage(event.currentTarget.value)}
+             onKeyPress={(event) => commitThought(event)}/>
     </div>
   );
 }
